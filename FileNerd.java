05f2765bc736362c2201fb1d472ddcb97d261d6f
TheControlFish <jacef314@gmail.com>
2016-10-26 12:39:10 -0600

    Add files via upload

---
+++ FileNerd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class FileNerd {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		Scanner sf = new Scanner(new File("C:\\temp_Larry\\NerdData.txt"));
+		String []a = new String[1000];
+		int maxIndx = -1;
+		while(sf.hasNext()){
+			maxIndx++;
+			a[maxIndx] = sf.nextLine();
+		}
+		for (int n = 0; n <= maxIndx; n++){
+			if (a[n].substring(0,3).equals("The")){
+				System.out.println(a[n]);
+			}
+		}
+	}
+
+}
